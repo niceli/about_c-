ca8a2c7f2a01bfd9a5266628efcc052693eeb9c3
niceli <874374995@qq.com>
2015-11-23 15:08:58 +0800

    add method 2 next_node_of_binary_tree.cpp

--- next_node_of_binary_tree.cpp
+++ next_node_of_binary_tree.cpp
@@ -37,3 +37,25 @@ public:
         return pNode;
     }
 };
+//method 2
+class Solution {
+public:
+    TreeLinkNode* GetNext(TreeLinkNode* pNode)
+    {
+        if(!pNode) return NULL;
+        if(pNode->right){
+            pNode = pNode->right;
+            while(pNode->left){
+                pNode = pNode->left;
+            }
+            return pNode;
+        }
+        while(pNode->next){
+            if(pNode->next->left == pNode){
+                return pNode->next;
+            }
+            pNode = pNode->next;
+        }
+        return NULL;
+    }
+};
